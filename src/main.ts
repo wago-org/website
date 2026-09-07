0b393122931878b51ab34ab3e948d250b7c027be
JairusSW <jairus@impart.security>
2026-09-07 09:46:10 -0400

    website: replace beta waitlist with Discord

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { initInstallSplit } from "./install-split.js";
 import { initSparkles } from "./sparkles.js";
 import { initScrollSpy } from "./scrollspy.js";
 import { initEditor } from "./editor.js";
-import { initBeta } from "./beta.js";
 import { initTheme } from "./theme.js";
 
 async function init(): Promise<void> {
@@ -25,7 +24,6 @@ async function init(): Promise<void> {
     initCopyButtons();
     initInstallSplit();
     initScrollSpy();
-    initBeta();
 }
 
 if (document.readyState === "loading") {
